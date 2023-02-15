f8cc9ff10323c6a81e29aef88db02f884b0f509e
LucasEfdl <lucasefdl@gmail.com>
2023-02-15 20:47:20 -0300

    resolvendo a questão dos alunos/gabarito

--- Listas_De_Algoritimos_II/2_Alocação dinâmica de vetores/Questao1.c
+++ Listas_De_Algoritimos_II/2_Alocação dinâmica de vetores/Questao1.c
@@ -4,30 +4,54 @@
 
 int main(void){
   setlocale(LC_ALL, "Portuguese");
-  int n;
-  float porFem = 0, porMac = 0;
-  printf("Quantas pessoas participaram da pesquisa?");
+  int n, i;
+  float qtd_mulheres = 0, qtd_homens = 0, likeGirl = 0, likeMan = 0;
+  float porFem, porMac;
+  printf("Quantas pessoas participaram da pesquisa?\n");
   scanf("%i", &n);
   char *sexo, *opniao;
   sexo = (char*) malloc(n * sizeof(char)); 
   opniao = (char*) malloc(n * sizeof(char)); 
 
-  for (int i = 0; i < n; i++){
-    printf("Digite o sexo e a opnião da %iº pessoa (M/F e G/NG)\n", i + 1);
-    scanf(" %c %c", &sexo[i], &opniao[i]);
-    if(sexo[i] == 'F' && opniao[i] == 'G'){
-      porFem++;
+  //Preenchendo os vetores
+  for (i = 0; i < n; i++)
+  {
+    printf(" 'M' se for homem, 'F' se for mulher\n'G' para gostei, 'N' para não gostei.\n");
+    printf("Digite o sexo e a opnião da %iº pessoa (M/F e G/N)\n", i + 1);
+    scanf(" %[^\n]s", &sexo[i]);
+    scanf(" %[^\n]s",&opniao[i]);
+  }
+
+  //Separando a quantidade de mulheres que gostaram da pesquisa e homens que não gostaram da pesquisa
+  for (i = 0; i < n; i++)
+  {
+    if(sexo[i] == 'F' && opniao[i] == 'G')
+    {
+      likeGirl++;
     }
-    else if(sexo[i] == 'M' && opniao[i] == 'NG'){
-      porMac++;
+    else if(sexo[i] == 'M' && opniao[i] == 'N')
+    {
+      likeMan++;
+    }
+  }
+  
+  //dividindo os entrevistados em homens e muçheres para calcular a pocentagem.
+  for(i=0; i<n; i++){
+    if(sexo[i] == 'F')
+    {
+      qtd_mulheres++;
+    }
+    if(sexo[i] == 'M')
+    {
+      qtd_homens++;
     }
   }
 
-  porFem /= 100;
-  porMac /= 100;
-
-  printf("%f%% das mulheres gostaram desse produto.", porFem); 
-  printf("%f%% dos homens não gostaram desse produto.", porMac); 
+  porFem = (likeGirl/qtd_mulheres)*100;
+  porMac = (likeMan/qtd_homens)*100;
+  
+  printf("%.1f%% das mulheres gostaram desse produto.\n", porFem); 
+  printf("%.1f%% dos homens não gostaram desse produto.", porMac); 
 
   free(sexo);
   free(opniao);
--- Listas_De_Algoritimos_II/2_Alocação dinâmica de vetores/Questao2.c
+++ Listas_De_Algoritimos_II/2_Alocação dinâmica de vetores/Questao2.c
@@ -1,1 +1,65 @@
-Em desenvolvimento...
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+  int qtdQuest, i, j, qtdAlunos, aprovados;
+  float pontosPorQuest, nota, porcAprovados;
+
+
+  printf("Quantas questoes tem a prova?\n");
+  scanf("%d", &qtdQuest);
+  pontosPorQuest = 10/qtdQuest;
+
+
+  //declarando e preenchendo o gabarito da prova
+  char * gabaritoProva = (char *) malloc(qtdQuest*sizeof(char));
+  printf("Digite o gabarito da prova (a, b, c, d):\n");
+  for ( i = 0; i < qtdQuest; i++){
+    scanf(" %c", &gabaritoProva[i]);
+  }
+
+
+  //Alocando o espaço dos alunos e suas respostas
+  printf("Quantos alunos fizeram a prova:\n");
+  scanf("%d", &qtdAlunos);
+  char ** respostas = (char **) malloc(qtdAlunos*sizeof(char*));
+  for (i = 0; i < qtdAlunos; i++){
+    respostas[i] = (char *) malloc(qtdQuest*sizeof(char));
+  }
+  
+
+  //capturando as respostados dos alunos e comparando as respostas
+  for (i = 0; i < qtdAlunos; i++){
+    int acerto = 0;
+    printf("Digite as respostado do %dª aluno:\n", i+1);
+    for (j = 0; j<qtdQuest; j++){
+      scanf(" %c", &respostas[i][j]);
+      if(respostas[i][j] == gabaritoProva[j]){
+        acerto++;
+      }
+    }
+    nota = pontosPorQuest * acerto;
+    if(nota >= 6){
+      aprovados++;
+    }
+    printf("A nota do %dª aluno foi: %.1f\n", i+1, nota);
+  }
+  
+  //Calculado a porcentagem de alunos aprovados
+  porcAprovados = (aprovados/qtdAlunos)*100;
+  printf("A porcentagem de alunos aprovados foi de: %.1f%%", porcAprovados);
+
+
+  //Liberando o espaço alocado dinamicamente
+  free(gabaritoProva);
+  for (i = 0; i < qtdAlunos; i++)
+  {
+    free(respostas[i]);
+  }
+  free(respostas);
+  
+  
+}
